654149c0e5adaa092d40be5e12e66834b0417dfe
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2022-08-11 07:30:28 +1000

    feat: git push shortcut

--- nvim/.config/nvim/lua/config/fugitive.lua
+++ nvim/.config/nvim/lua/config/fugitive.lua
@@ -1,3 +1,4 @@
-vim.keymap.set('n', '<leader>gg', ':G<cr>')
-vim.keymap.set('n', '<leader>g<space>', ':G<space>')
-vim.keymap.set('n', '<leader>gb', ':G blame<cr>')
+vim.keymap.set('n', '<leader>gg', ":G<cr>")
+vim.keymap.set('n', '<leader>g<space>', ":G<space>")
+vim.keymap.set('n', '<leader>gb', ":G blame<cr>")
+vim.keymap.set('n', '<leader>gp', ":G push<cr>")
