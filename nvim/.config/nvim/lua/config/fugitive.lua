498425bd4582aa65a94b495e55bef4067c1b4106
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2022-09-07 09:26:21 +1000

    feat: more git keybindings

--- nvim/.config/nvim/lua/config/fugitive.lua
+++ nvim/.config/nvim/lua/config/fugitive.lua
@@ -1,4 +1,7 @@
 vim.keymap.set('n', '<leader>gg', ":G<cr>")
 vim.keymap.set('n', '<leader>g<space>', ":G<space>")
 vim.keymap.set('n', '<leader>gb', ":G blame<cr>")
-vim.keymap.set('n', '<leader>gp', ":G push<cr>")
+vim.keymap.set('n', '<leader>gl', ":G log<cr>")
+vim.keymap.set('n', '<leader>gph', ":G push<cr>")
+vim.keymap.set('n', '<leader>gpl', ":G pull<cr>")
+vim.keymap.set('n', '<leader>gpr', ":G pull --rebase origin main<cr>")
--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -21,4 +21,4 @@ require("telescope").setup({
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-vim.keymap.set('n', '<leader>gc', '<cmd>Telescope git_commits<cr>')
+vim.keymap.set('n', '<leader>gc', '<cmd>Telescope git_bcommits<cr>')
