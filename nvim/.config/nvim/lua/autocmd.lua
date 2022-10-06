8d8c9ea865ddce390f1f08b103c9854a3d1e7495
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2022-10-07 08:12:55 +1100

    chore: add some filetypes

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -6,7 +6,7 @@ local TabSize = api.nvim_create_augroup('TabSize', { clear = true })
 api.nvim_create_autocmd('FileType', {
   pattern = 'rust',
   group = TabSize,
-  callback = function ()
+  callback = function()
     ol.expandtab = true
     bo.tabstop = 4
     bo.shiftwidth = 4
@@ -19,4 +19,15 @@ api.nvim_create_autocmd('BufWritePost', {
   pattern = 'plugins.lua',
   group = Packer,
   command = 'so ~/.config/nvim/lua/plugins.lua | PackerCompile',
+})
+
+-- Make astro files actually astro
+api.nvim_create_autocmd({ 'BufRead', 'BufEnter' }, {
+  pattern = '*.astro',
+  command = 'set filetype=astro',
+})
+-- Make sure all env files have the right file type
+api.nvim_create_autocmd({ 'BufRead', 'BufEnter' }, {
+  pattern = '.env.*',
+  command = 'set filetype=sh',
 })
