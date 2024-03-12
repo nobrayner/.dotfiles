7c86d72b65fcf505089ea90f202f3f4fb0b656c9
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2024-03-13 08:36:17 +1100

    stuff

--- bspwm/.config/bspwm/toggle-stream-mode.sh
+++ bspwm/.config/bspwm/toggle-stream-mode.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# 440 is based on turning an ultrawide (3440) into 16:9
-if [[ $(bspc config -m DP-2 left_padding) -eq 0 ]]; then
-  bspc config -m DP-2 left_padding 440
-  bspc config -m DP-2 right_padding 440
+# 880 is based on turning an ultrawide (3440) into 16:9, only using the left side
+if [[ $(bspc config -m DP-0 right_padding) -eq 0 ]]; then
+  bspc config -m DP-0 right_padding 880
 else
-  bspc config -m DP-2 left_padding 0
-  bspc config -m DP-2 right_padding 0
+  bspc config -m DP-0 right_padding 0
 fi
+
