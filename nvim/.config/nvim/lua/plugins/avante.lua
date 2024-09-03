d690a0b71dea3c039b553370bb8af5fe8fac826a
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2024-09-03 10:22:53 +0200

    chore(nvim): basics for avante

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,23 @@
+return {
+	-- Has some issues where it messes with styles, and cannot speak with the whole project yet. But definitely worth coming back to.
+	-- Especially with a locally running LLM
+	-- "yetone/avante.nvim",
+	-- event = "VeryLazy",
+	-- opts = {
+	--   -- add any opts here
+	-- },
+	-- dependencies = {
+	--   "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+	--   "stevearc/dressing.nvim",
+	--   "nvim-lua/plenary.nvim",
+	--   "MunifTanjim/nui.nvim",
+	--   --- The below is optional, make sure to setup it properly if you have lazy=true
+	--   {
+	--     'MeanderingProgrammer/render-markdown.nvim',
+	--     opts = {
+	--       file_types = { "markdown", "Avante" },
+	--     },
+	--     ft = { "markdown", "Avante" },
+	--   },
+	-- },
+}
